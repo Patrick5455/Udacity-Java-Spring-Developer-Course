7a3334d1b71dddf502e6478445a477dcfe3f9715
Patrick5455 <patrickojunde@gmail.com>
2020-06-05 14:19:49 +0100

    Create Student Grade Table with Controlller StudentList

---
+++ freemarker/src/main/java/com/example/freemarker/controller/StudentList.java
@@ -0,0 +1,27 @@
+package com.example.freemarker.controller;
+
+import com.example.freemarker.model.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class StudentList {
+
+    @RequestMapping("/list")
+    public String list (Model model){
+
+        List <Student> studentList = new ArrayList<>();
+
+        studentList.add(new Student(1, "Patrick", 90));
+        studentList.add(new Student(2, "Stephen", 95));
+        studentList.add(new Student(3, "Boluwatife", 97));
+        studentList.add(new Student(4, "Florence", 99));
+
+        return "list";
+    }
+
+}
