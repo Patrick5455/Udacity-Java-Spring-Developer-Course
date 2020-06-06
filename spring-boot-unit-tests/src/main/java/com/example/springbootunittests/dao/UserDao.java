4e110957e275d44be6e91ab10b9aa74f7d0d8233
Patrick5455 <patrickojunde@gmail.com>
2020-06-06 14:16:51 +0100

    Start Spring Boot Unit Tests

---
+++ spring-boot-unit-tests/src/main/java/com/example/springbootunittests/dao/UserDao.java
@@ -0,0 +1,14 @@
+package com.example.springbootunittests.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDao {
+
+    public String getUser (){
+
+        return "Tom";
+    }
+
+
+}
