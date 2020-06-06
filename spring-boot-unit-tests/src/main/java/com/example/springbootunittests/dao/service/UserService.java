a0a93b6334e941c6603b9b4b7cc88b5093ddec84
Patrick5455 <patrickojunde@gmail.com>
2020-06-06 14:28:53 +0100

    Add Test Class

---
+++ spring-boot-unit-tests/src/main/java/com/example/springbootunittests/dao/service/UserService.java
@@ -0,0 +1,18 @@
+package com.example.springbootunittests.dao.service;
+
+import com.example.springbootunittests.dao.UserDao;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class UserService {
+
+    @Resource
+    private UserDao userDao;
+
+    public String getUser(){
+        return userDao.getUser();
+    }
+
+}
