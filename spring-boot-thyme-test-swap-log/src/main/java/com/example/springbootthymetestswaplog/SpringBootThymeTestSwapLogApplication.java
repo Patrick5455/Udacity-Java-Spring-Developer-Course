8e6b350730da33a8d4023ff7cd23199cc8e72dc2
Patrick5455 <patrickojunde@gmail.com>
2020-06-06 19:23:40 +0100

    Putting it all together: thymeLeaf test swap logging

---
+++ spring-boot-thyme-test-swap-log/src/main/java/com/example/springbootthymetestswaplog/SpringBootThymeTestSwapLogApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootthymetestswaplog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootThymeTestSwapLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootThymeTestSwapLogApplication.class, args);
+	}
+
+}
---
+++ spring-boot-thyme-test-swap-log/src/test/java/com/example/springbootthymetestswaplog/SpringBootThymeTestSwapLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootthymetestswaplog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootThymeTestSwapLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
