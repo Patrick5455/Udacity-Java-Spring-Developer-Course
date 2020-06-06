bbc4d320d396b98873218abd030cdff38f14d8b5
Patrick5455 <patrickojunde@gmail.com>
2020-06-06 17:01:41 +0100

    Add spring-boot-starter-log4j2 dependency

---
+++ spring-boot-logging/src/main/java/com/example/springbootlogging/contoller/LoggingController.java
@@ -0,0 +1,29 @@
+package com.example.springbootlogging.contoller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class LoggingController {
+
+    Logger logger = LoggerFactory.getLogger(LoggingController.class);
+
+    @RequestMapping("log")
+    public Map<String, Object> log (){
+        Map<String, Object> map = new HashMap<>();
+        map.put("Name", "Patrick Ojunde,");
+        map.put("Occupation", "Software & Data ENgineer");
+
+        logger.warn("This is a warning message");
+        logger.info("This is logger logging info");
+        logger.debug("This is logger logging debug message");
+        return map;
+    }
+
+
+}
