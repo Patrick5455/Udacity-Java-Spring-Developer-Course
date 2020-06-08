c511953ade8f4c4433ad8129c7b42de9e5e7dd2c
Patrick5455 <patrickojunde@gmail.com>
2020-06-08 20:17:38 +0100

    Add validation comments to model.User class

--- spring-boot-validation/src/main/java/com/example/springbootvalidation/model/User.java
+++ spring-boot-validation/src/main/java/com/example/springbootvalidation/model/User.java
@@ -8,10 +8,20 @@ import javax.validation.constraints.NotBlank;
 
 public class User {
 
+    /* NB :
+    Common validation annotations:
+    @NotBlank: check if string is null or empty after trimmed the front and end spaces.
+    @NotEmpty: check if string is null or empty without trim the front and end spaces.
+    @Length: check string length, include max and min.
+    @Min: check min, cannot be less. for instance if @Min(0), then input should not less than 0.
+    @Max: check max, cannot be over.
+    @Email: check email format, should be XX@XX.XX
+     */
+
 
     @NotBlank(message = "name cannot be blank")
     private String name;
-//    @NotBlank(message = " password cannot be blank")
+    @NotBlank(message = " password cannot be blank")
     @Length(min = 6, max = 10, message = "password must be between 6 and 10 characters")
     private String password;
     @Min(value = 0, message = "grade cannot be less than 0")
