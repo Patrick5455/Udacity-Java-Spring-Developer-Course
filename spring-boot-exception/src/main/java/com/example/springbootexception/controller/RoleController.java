50ce493384dbcfe60e310c96ca428c489d9cc3ef
Patrick5455 <patrickojunde@gmail.com>
2020-06-13 21:01:10 +0100

    Add Implementation of @ControllerAdvise for handling global exceptions > Created exceptions package for class to use  @ControllerAdvise + @ExceptionHandler() for global exceptions

---
+++ spring-boot-exception/src/main/java/com/example/springbootexception/controller/RoleController.java
@@ -0,0 +1,17 @@
+package com.example.springbootexception.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RoleController {
+
+    @RequestMapping("/roleadd")
+    public String anotherMathError(){
+
+        int num = 10/0;
+        return "add";
+    }
+
+
+}
