aa39c0ca12ce68076134d44d7ff2d8f24055dbf7
Patrick5455 <patrickojunde@gmail.com>
2020-06-13 20:35:57 +0100

    Add use of @Exceptionhandler() to handle exception

--- spring-boot-exception/src/main/java/com/example/springbootexception/controller/ControllerException.java
+++ spring-boot-exception/src/main/java/com/example/springbootexception/controller/ControllerException.java
@@ -3,16 +3,26 @@ package com.example.springbootexception.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ControllerException {
 
-
-
+// using custom error page to handle this endpoint exception
     @RequestMapping("/add")
     public String handleMathErrorException (){
         int num = 10/0;
        return "add";
+    }
+
+
+    @RequestMapping("/add2")
+    @ExceptionHandler(value = {java.lang.ArithmeticException.class})
+    public ModelAndView handleException(Exception e){
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("Error", e.toString());
+        mv.setViewName("mathError");
+        return mv;
     }
 
 
