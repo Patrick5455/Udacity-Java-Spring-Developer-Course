88b61330eab43e4a7736b1cadeb12a08fe2674b6
Patrick5455 <patrickojunde@gmail.com>
2020-06-13 21:08:46 +0100

    Implement GLobal Exception Handling > Create Package exception > Add class coantaining @ControllerAdvise + ExceptionHandler() for gloabl exception handling > Comment out @ExceptionHandling in classes without @ControllerAdvise to avoid conflict

--- spring-boot-exception/src/main/java/com/example/springbootexception/exceptions/GlobalExceptionControllerHandler.java
+++ spring-boot-exception/src/main/java/com/example/springbootexception/exceptions/GlobalExceptionControllerHandler.java
@@ -1,7 +1,30 @@
 package com.example.springbootexception.exceptions;
 
 
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice
 public class GlobalExceptionControllerHandler {
+
+    @ExceptionHandler(value = {java.lang.ArithmeticException.class})
+    public ModelAndView handleGlobalMathExceptions (Exception e){
+
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("error", e.toString());
+        mv.setViewName("mathError");
+        return mv;
+    }
+
+    @ExceptionHandler(value = {java.lang.NullPointerException.class})
+    public ModelAndView handleGlobalNullExceptions (Exception e){
+
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("error", e);
+        mv.setViewName("nullError");
+        return mv;
+    }
 
 
 }
