ee39944cc5fc95e493f0cf87f7ffccf709d612a6
Patrick5455 <patrickojunde@gmail.com>
2020-06-15 13:19:05 +0100

    Add Models > User Class > UserResponse Class

---
+++ spring-boot-websocket/src/main/java/com/example/springbootwebsocket/model/UserResponse.java
@@ -0,0 +1,17 @@
+package com.example.springbootwebsocket.model;
+
+public class UserResponse {
+
+    private String content;
+
+    public UserResponse (){}
+
+    public void setContent(String content){
+        this.content = content;
+    }
+
+    public String getContent (){
+
+        return content;
+    }
+}
