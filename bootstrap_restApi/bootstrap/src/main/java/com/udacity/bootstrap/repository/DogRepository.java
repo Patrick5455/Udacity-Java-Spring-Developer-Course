4c95c26d0a9bba3a61503f2203ece07cae6c3980
Patrick5455 <patrickojunde@gmail.com>
2020-06-22 21:07:05 +0100

    Create DogNotFOundException Class and use OPtional to avoid null objects

--- bootstrap_restApi/bootstrap/src/main/java/com/udacity/bootstrap/repository/DogRepository.java
+++ bootstrap_restApi/bootstrap/src/main/java/com/udacity/bootstrap/repository/DogRepository.java
@@ -2,15 +2,17 @@ package com.udacity.bootstrap.repository;
 
 
 import com.udacity.bootstrap.Entity.Dog;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface DogRepository extends CrudRepository<Dog,Long>  {
+public interface DogRepository extends JpaRepository<Dog,Long> {
 
     @Query("select d.id, d.breed from Dog d where d.id=:id")
-    String findBreedById(Long id);
+    Optional<String> findBreedById(Long id);
 
     @Query("select d.id, d.breed from Dog d")
     List<String> findAllBreed();
--- bootstrap_restApi/bootstrap/src/main/java/com/udacity/bootstrap/service/DogServiceImpl.java
+++ bootstrap_restApi/bootstrap/src/main/java/com/udacity/bootstrap/service/DogServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import javax.servlet.http.PushBuilder;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DogServiceImpl implements DogService{
@@ -15,8 +16,24 @@ public class DogServiceImpl implements DogService{
     public DogRepository dogRepository;
 
     public String retrieveDogBreedById(Long id){
-        return dogRepository.findBreedById(id);
+        Optional<String> optionalDog = dogRepository.findBreedById(id);
+        if (!optionalDog.isPresent()){
+            throw new DogNotFoundException();
+        }
+        else return optionalDog.get();
     }
+
+    // Alternative implementation of the above method
+    // NB: to use this alternative implementation,
+    // we unwrap the the Optional class from the  method return type of String
+
+//    public String retrieveDogBreedById(Long id) {
+//        Optional<String> optionalBreed = Optional.ofNullable(dogRepository.findBreedById(id));
+//        String breed = optionalBreed.orElseThrow(DogNotFoundException::new);
+//        return breed;
+//    }
+
+
 
     public List<String> retrieveDogBreeds(){
         return dogRepository.findAllBreed();
